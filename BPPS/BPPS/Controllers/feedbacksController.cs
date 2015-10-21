d09850c57f278bc7aa26a5e0185159ab29b3dc88
Zuzka <zuzka.papalova@gmail.com>
2015-10-21 19:32:28 +0200

    MatejProject

---
+++ BPPS/BPPS/Controllers/feedbacksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BPPS.Models;
+
+namespace BPPS.Controllers
+{
+    public class feedbacksController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: feedbacks
+        public ActionResult Index()
+        {
+            return View(db.feedbacks.ToList());
+        }
+
+        // GET: feedbacks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            feedbacks feedbacks = db.feedbacks.Find(id);
+            if (feedbacks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedbacks);
+        }
+
+        // GET: feedbacks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: feedbacks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "feedback_id,initiated,received,project_id,Id")] feedbacks feedbacks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.feedbacks.Add(feedbacks);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(feedbacks);
+        }
+
+        // GET: feedbacks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            feedbacks feedbacks = db.feedbacks.Find(id);
+            if (feedbacks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedbacks);
+        }
+
+        // POST: feedbacks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "feedback_id,initiated,received,project_id,Id")] feedbacks feedbacks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(feedbacks).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(feedbacks);
+        }
+
+        // GET: feedbacks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            feedbacks feedbacks = db.feedbacks.Find(id);
+            if (feedbacks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedbacks);
+        }
+
+        // POST: feedbacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            feedbacks feedbacks = db.feedbacks.Find(id);
+            db.feedbacks.Remove(feedbacks);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
